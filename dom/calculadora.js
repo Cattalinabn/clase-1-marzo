5f94fbed59a211bca8ec6a97a2b241dae2ffa8a4
Cattalinabn <115637472+Cattalinabn@users.noreply.github.com>
2023-04-12 08:26:31 -0500

    Add files via upload
    calculadora

---
+++ dom/calculadora.js
@@ -0,0 +1,16 @@
+function sum() {
+ let v_nro1 = parseFloat(document.getElementById("nr1").value)
+ let v_nro2 = parseFloat(document.getElementById("nr2").value)
+ let suma = v_nro1 + v_nro2
+ document.getElementById("resultado").innerHTML = suma
+}
+function resta(){
+let v_nro1 = parseFloat(document.getElementById("nr1").value)
+let v_nro2 = parseFloat(document.getElementById("nr1").value)
+let resta = v_nro1 - v_nro2
+document.getElementById("resultado").innerHTML = resta
+}
+
+
+
+ 
